0106ac417ed362deb8410a28cbf7421447d8ffbb
Sasha Volmanov <ssashavol7@gmail.com>
2015-06-24 21:10:11 +0400

    New Reposit Html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
+"use sctrict";
+
 var gulp = require("gulp"),
 		connect = require("gulp-connect"),
+    concatCss = require('gulp-concat-css'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    minifyCss = require('gulp-minify-css'),
 		opn = require("opn");
 
 // Запускаем локальный сервер
@@ -12,29 +18,41 @@ gulp.task('connect', function() {
   opn('http://localhost:8888');
 });
 
+//LESS compiler
+gulp.task('less', function () {
+  gulp.src('app/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('app/css/'));
+});
+
 // Работа с HTML
 gulp.task('html', function () {
-  gulp.src('./app/*.html')
+  gulp.src('app/*.html')
     .pipe(connect.reload());
 });
 
 // Работа с CSS
 gulp.task('css', function () {
-  gulp.src('./app/css/*.css')
+  gulp.src('app/css/*.css')
+  .pipe(concatCss("concat.css"))
+  .pipe(minifyCss(""))
+   .pipe(rename("concat.min.css"))
+   .pipe(gulp.dest('app/css'))
     .pipe(connect.reload());
 });
 
 // Работа с JS
 gulp.task('js', function () {
-  gulp.src('./app/js/*.js')
+  gulp.src('app/js/*.js')
     .pipe(connect.reload());
 });
 
 // Слежка
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html']);
-  gulp.watch(['./app/js/*.js'], ['js']);
-  gulp.watch(['./app/css/*.css'], ['css']);
+  gulp.watch(['app/*.html'], ['html']);
+  gulp.watch(['app/js/*.js'], ['js']);
+  gulp.watch(['app/css/*.css'], ['css']);
+  gulp.watch(['app/less/*.less'], ['less']);
 });
 
 // Задача по-умолчанию
